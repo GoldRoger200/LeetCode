8057f940a341bbc75a98cfe26eec4627754649e0
Valir69 <104688563+Valir69@users.noreply.github.com>
2022-10-16 12:01:59 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (61.38%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i=digits.size()-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            else{
+                digits[i]=0;
+            }
+        }
+        vector<int>v(digits.size()+1,0);
+        v[0]=1;
+        return v;
+    }
+};
